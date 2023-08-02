41640306a55f67903d80a2250cee8c1ff200710d
faresh9 <93577791+faresh9@users.noreply.github.com>
2023-08-02 16:55:38 +0300

    same poor error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import harvardArt from './data/harvardArt';
-import GalleryNavigation from './components/GalleryNavigation';
-import GalleryView from './components/GalleryView';
-import HomePage from './components/HomePage';
-import PageNotFound from './components/PageNotFound';
+// src/App.js
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import harvardArt from "./data/harvardArt";
+import GalleryNavigation from "./components/GalleryNavigation/GalleryNavigation";
+import GalleryView from "./components/GalleryView/index";
+import HomePage from "./components/HomePage/HomePage";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   const findGalleryById = (galleryId) => {
@@ -17,16 +18,17 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className="page-wrapper">
-        <GalleryNavigation galleries={harvardArt.records} />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/galleries/:galleryId" render={GalleryViewWithGallery} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="page-wrapper">
+      <GalleryNavigation galleries={harvardArt.records} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route
+          path="/galleries/:galleryId"
+          component={GalleryViewWithGallery}
+        />
+        <Route component={PageNotFound} />
+      </Switch>
+    </div>
   );
 }
 
